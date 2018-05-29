33243f18ebd5c64fd27d9edbaef85075286b35ce
asophiel <jairoburvile@gmail.com>
2018-05-29 18:23:02 -0300

    Conversor de farenheit a celsius agregado

--- conversor_farenheit_a_celsius.py
+++ conversor_farenheit_a_celsius.py
@@ -1,7 +1,9 @@
 
 farenheit = input("Dime el valor en farenheit que quieres convertir en celsius")
 
-celsius = (int(farenheit) - int(32)) / float(1.8)
+celsius = ((int(farenheit) - (int(32))) / float(1.8))
 
-print(celsius)
+print("El resultado es {}".format(celsius))
+
+
 
